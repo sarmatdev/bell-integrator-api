ac9878affe277a3967910ca864cebf7770741848
Sarmat Khachirov <skhachirov12@gmail.com>
2021-01-18 11:38:12 +0300

    fix: prep

--- src/app.ts
+++ src/app.ts
@@ -28,6 +28,6 @@ app.get('/ping', (req: Request, res: Response) => {
   res.status(200).send('pong')
 })
 
-app.listen(3000, () => {
-  console.log(`🚀 Server is running on port 3000`)
+app.listen(5000, () => {
+  console.log(`🚀 Server is running on port 5000`)
 })
